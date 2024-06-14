d2fc2113b7233d475ee6f872e382759165c6b22f
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-13 22:47:08 -0300

    User Entity and Controller

---
+++ src/main/java/jeffersonrolino/com/github/spring_store/controllers/UserController.java
@@ -0,0 +1,19 @@
+package jeffersonrolino.com.github.spring_store.controllers;
+
+import jeffersonrolino.com.github.spring_store.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@gmail.com", "41997452344", "123");
+        return ResponseEntity.ok().body(user);
+    }
+
+}
