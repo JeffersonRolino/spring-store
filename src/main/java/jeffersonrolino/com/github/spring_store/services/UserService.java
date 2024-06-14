9ee5d9f00abc3316edc75de1fcb2908d00b390b9
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-14 01:34:05 -0300

    Service layer, component registration

---
+++ src/main/java/jeffersonrolino/com/github/spring_store/services/UserService.java
@@ -0,0 +1,26 @@
+package jeffersonrolino.com.github.spring_store.services;
+
+import jeffersonrolino.com.github.spring_store.entities.User;
+import jeffersonrolino.com.github.spring_store.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.get();
+    }
+}
