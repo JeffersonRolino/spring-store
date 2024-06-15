687157618a9637ff175b41026e65cee7f8cf8e2a
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-15 11:31:32 -0300

    OrderStatus enum

--- src/main/java/jeffersonrolino/com/github/spring_store/config/TestConfig.java
+++ src/main/java/jeffersonrolino/com/github/spring_store/config/TestConfig.java
@@ -2,6 +2,7 @@ package jeffersonrolino.com.github.spring_store.config;
 
 import jeffersonrolino.com.github.spring_store.entities.Order;
 import jeffersonrolino.com.github.spring_store.entities.User;
+import jeffersonrolino.com.github.spring_store.entities.enums.OrderStatus;
 import jeffersonrolino.com.github.spring_store.repositories.OrderRepository;
 import jeffersonrolino.com.github.spring_store.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(user1, user2));
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
