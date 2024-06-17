6f6d9efc5713bc312886f45b3afcd711ea7c7bc6
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-17 02:26:51 -0300

    Product-OrderItem one-to-many association

--- src/main/java/jeffersonrolino/com/github/spring_store/entities/Product.java
+++ src/main/java/jeffersonrolino/com/github/spring_store/entities/Product.java
@@ -1,5 +1,6 @@
 package jeffersonrolino.com.github.spring_store.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -22,6 +23,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -76,6 +80,15 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> orders = new HashSet<>();
+        for(OrderItem orderItem : items){
+            orders.add(orderItem.getOrder());
+        }
+        return orders;
     }
 
     @Override
